13f0829b4ea7f255c598729f27abbe55f6186d26
55555moon <492482968@qq.com>
2017-05-16 10:48:18 +0800

    2

---
+++ git001/src/com/zwy/action/AVB.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)AVB.java
+ * Description:	TODO 填写文件作用简要说明
+ * Version :	0.0.0
+ * Copyright:	Copyright (c) 哈尔滨修盾信息科技有限公司  版权所有
+ * Create by:	赵武岳  2017年5月16日
+ */
+package com.zwy.action;
+
+/**
+ * TODO 填写功能说明
+ * @author 赵武岳
+ */
+public class AVB {
+
+}
